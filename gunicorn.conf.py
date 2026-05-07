4d29c0f3d0084a8998ac3f796e19f3c30d8b56c5
Muhammed Jasil E K <ekjasil2008@gmail.com>
2026-05-07 13:28:49 +0530

    heyyyy render, stop crashing my server due to low ram

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,7 +1,8 @@
-import multiprocessing
+# import multiprocessing
 
 bind = "0.0.0.0:8000"
-workers = multiprocessing.cpu_count() * 2 + 1
+# workers = multiprocessing.cpu_count() * 2 + 1
+workers = 1
 worker_class = "uvicorn.workers.UvicornWorker"
 loglevel = "warning"
 accesslog = "-"
